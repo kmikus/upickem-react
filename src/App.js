9a908b3f524e286eff81e6966ea73955f4b82b33
Kevin Mikus <kevinpmikus@gmail.com>
2018-12-22 13:27:54 -0500

    refactored login page to own file

--- src/App.js
+++ src/App.js
@@ -1,67 +1,15 @@
 import React, { Component } from 'react';
-import axios from 'axios';
-import logo from './logo.svg';
+import LoginPage from './pages/login'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <LoginHeading />
-        <div className="container">
-          <LoginForm />
-        </div>
+        <LoginPage />
       </div>
     );
   }
 }
-
-const LoginHeading = (props) => {
-  return (
-    <h1 className="jumbotron">Welcome to uPickEm</h1>
-  );
-};
-
-class LoginForm extends Component {
-  state={
-    username: '',
-    password: ''
-  };
-
-  login = (event) => {
-    event.preventDefault();
-    axios.get('https://jsonplaceholder.typicode.com/todos/1')
-      .then((response) => console.log(response.data));
-    console.log('Clicked');
-  };
-
-  render() {
-    return (
-      <div style={{margin: '10%'}}>
-        <form onSubmit={this.login}>
-
-          <div className="form-group">
-            <input 
-              value={this.state.username} onChange={(event) => this.setState({username: event.target.value})}
-              className="form-control" placeholder="Username or email"
-            />
-          </div>
-          <div className="form-group">
-            <input value={this.state.password} onChange={(event) => this.setState({password: event.target.value})}
-              className="form-control" placeholder="Password" type="password" />
-          </div>
-
-          <div className="form-group">
-            <input className="form-control btn btn-primary" type="submit" value="Login"/>
-          </div>
-
-          <div className="form-group">
-            <button className="form-control btn btn-outline-secondary">Register</button>
-          </div>
-        </form>
-      </div>
-    );
-  };
-};
 
 export default App;
